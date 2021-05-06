a5d65c79583a9e2ba0651c1071f8db227d1b2525
seymafirat <seymanurfiratt@gmail.com>
2021-05-06 22:40:48 +0300

    Day4 HW2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Customer customer = new Customer(1,"Þeyma Nur","Fýrat","30650092780",1998);
+		Customer customer = new Customer(1,"Þeyma Nur","Fýrat","30655588899",1998);
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MerniceServiceAdapter());
 		customerManager.Save(customer);
 	}
